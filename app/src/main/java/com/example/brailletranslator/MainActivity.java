824cd7775155c18620e5be772b1c76a7cd6e91dd
Elias Akl <128466689+3akll@users.noreply.github.com>
2026-06-14 15:08:18 +0200

    Java variable connected to XML elements

--- app/src/main/java/com/example/brailletranslator/MainActivity.java
+++ app/src/main/java/com/example/brailletranslator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.brailletranslator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Connect each Java variable to its XML element
+        EditText inText = findViewById(R.id.inputText);
+        Button tButton = findViewById(R.id.translateButton);
+        TextView outText = findViewById(R.id.outputText);
+
+        // Add a Button Click Listener that translates the input to the braille result when clicked
+        tButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textToTranslate = inText.getText().toString();
+                outText.setText(textToTranslate);
+            }
         });
     }
 }
